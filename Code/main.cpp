2d5fe143b2639e4208b6f99fbc566d21f6705f43
Milo46 <56130120+Milo46@users.noreply.github.com>
2020-02-24 20:13:13 +0100

    Create main.cpp

---
+++ Code/main.cpp
@@ -0,0 +1,16 @@
+//Game of Life - SFML
+//Napisalem to, poniewaz zaciekawilem sie ta Gra w Zycie
+//Moze na poczatku ta symulacja wydawala mi sie bezuzyteczna, ale sie przekonalem, ze moze byc uzyteczna :)
+//Np. do wyznaczania liczb pierwszych
+
+//Tylko dla Visual Studio, powoduje, ze konsola sie nie wyswietla
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+
+#include "game.h"
+
+void main()
+{
+	//Stworzenie obiektu typu Game i zastartowanie gry
+	Game game;
+	game.run();
+}
